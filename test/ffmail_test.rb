bd9217f508ee208f90d56830e2b97c9d6423ec81
eLobato <elobatocs@gmail.com>
2012-04-11 22:32:31 -0700

    Added attribute accessors stub and tests

--- test/ffmail_test.rb
+++ test/ffmail_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
+require 'fixtures/sample_mail'
 
 class FfmailTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Ffmail
+  test "sample mail has name and email as attributes accessible" do
+    sample = SampleMail.new
+    sample.name = "User"
+    assert_equal "User", sample.name
+    sample.email = "user@example.com"
+    assert_equal "user@example.com", sample.email
   end
 end
