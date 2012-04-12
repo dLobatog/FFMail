656851737c9b4f09a98accd56fe2dee5f602f1e7
Daniel Lobato Garcia <elobatocs at google.com>
2012-04-11 22:21:39 -0700

    Enginex gem basic structure

---
+++ lib/ffmail.rb
@@ -0,0 +1,2 @@
+module Ffmail
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '9a69a25ad6a498ccacdd38511ab897359e9663999166174aef78c6ca6f64d58624521d4172e01518b04eb8cebe756412be53a827d020968e9efe2a30c80c3bf6'
