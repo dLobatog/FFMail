ef244f0d6bb658e1f282bd45c961ba2fc29fffa7
eLobato <elobatocs@gmail.com>
2012-04-11 22:37:40 -0700

    Inherited clear_ from ActiveModel::Attribute-Methods

--- lib/ffmail/base.rb
+++ lib/ffmail/base.rb
@@ -1,7 +1,18 @@
 module Ffmail
   class Base
+    include ActiveModel::AttributeMethods
+    
+    attribute_method_prefix 'clear_'
+
     def self.attributes(*names)
       attr_accessor *names  
+
+      define_attribute_methods names
+    end
+
+    protected
+    def clear_attribute(attribute)
+      send("#{attribute}=", nil)  
     end
   end
 end
--- test/ffmail_test.rb
+++ test/ffmail_test.rb
@@ -9,4 +9,17 @@ class FfmailTest < ActiveSupport::TestCase
     sample.email = "user@example.com"
     assert_equal "user@example.com", sample.email
   end
+
+  test "sample mail can clear attributes with _clear prefix" do
+    sample = SampleMail.new
+    sample.name = "User"
+    assert_equal "User", sample.name
+    sample.email = "user@example.com"
+    assert_equal "user@example.com", sample.email  
+    sample.clear_name
+    assert_nil sample.name
+    sample.clear_email
+    assert_nil sample.email
+  end
+  
 end
